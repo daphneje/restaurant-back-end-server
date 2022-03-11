a10c936a04bace8b8a6c25d1f4725dd108bd05e1
daphneje <91236787+daphneje@users.noreply.github.com>
2022-03-12 05:04:43 +0800

    updated controllers with err catch block in async function

--- controllers/order-item.controller.js
+++ controllers/order-item.controller.js
@@ -3,37 +3,42 @@ const orderItemService = require("../services/order-item.service");
 
 class OrderItemController {
   async newOrderItem(req, res, next) {
-
-    // console.log(`typeof req.params.orderId ${typeof req.params.orderId}`);
-    // if (typeof req.params.orderId !== "number") {
-    //   res.status(400); // bad request
-    //   return res.json({ message: "Incorrect request data" });
-    // }
-
+    try{
     // Consume the service layer
     const result = await orderItemService.newOrderItem(req.body.orderId, req.body.createOrderItem);    
     res.status(result.status);
 
     // Return results from service
     return res.json({ data: result.data, message: result.message });
+    } catch (error) {
+    return res.status(500).json(error);
+    }
   }
 
   async orderItemDetails(req,res,next) {
+    try{
     const result = await orderItemService.orderItemDetails(req.params.orderItemId)
     res.status(result.status);
     console.log (result);
 
     //Return results
     return res.json({data: result.data, message: result.message});
+    } catch (error) {
+    return res.status(500).json(error);
+    }
   }
 
   async orderItemsInOrder(req,res,next) {
+    try{
     const result = await orderItemService.orderItemsInOrder(req.params.orderId)
     res.status(result.status);
     console.log (result);
 
     //Return results
     return res.json({data: result.data, message: result.message});
+    } catch (error) {
+    return res.status(500).json(error);
+    }
   }
 
 }
