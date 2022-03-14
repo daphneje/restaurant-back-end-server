4d07cfad9bef2a4e6e89cf64825e58c71b851e5d
daphneje <91236787+daphneje@users.noreply.github.com>
2022-03-15 03:44:28 +0800

    moved cors from server.js to routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const { testConnection } = require('../models');
+testConnection();
+
+const corsOptions = {
+  origin: "http://localhost:3000"
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to restaurant application back-end server." });
+});
 
 const orderItemRoutes = require('./order-item.routes');
 const orderRoutes = require('./order.routes');
